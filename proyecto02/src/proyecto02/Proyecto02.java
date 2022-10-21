fffed9bb9f65b7561058de178c0686e23ea23361
Alex Ramírez <alexis.rj2110@gmail.com>
2022-10-21 12:06:50 -0500

    Proyecto02-Taller02

--- proyecto02/src/proyecto02/Proyecto02.java
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -16,9 +16,9 @@ public class Proyecto02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Ejemplo dos básico de Java" +
-"Versionado a través de Git" +
-"Manejado desde GitHub" +
+        System.out.println("Ejemplo dos básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
 "Además se usar GitHubClassroom");
     }
     
